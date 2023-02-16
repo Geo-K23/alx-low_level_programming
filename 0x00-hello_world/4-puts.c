67f0b0935476d4538b8d24ab2373306b10041940
George Kazibwe <gkazibwe@gmail.com>
2023-02-16 07:45:40 -0800

    C program that prints using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/**
+ * The program uses the function puts to print a given sentence
+ * Description:function main prints the string assigned to puts()
+ * return value of main is 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -4,3 +4,4 @@ This README.md is for 0x00-hello_world directory with the following scripts
 1-compiler compiles a c file but does not link
 2-assembler generates the assembly code of a c file
 3-name compiles the c file and assigns it a given name
+4-puts is a C program that prints words using the function puts
