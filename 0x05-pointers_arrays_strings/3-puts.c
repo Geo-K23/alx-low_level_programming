e16901da2e0dbc5b5474686c7681ddba1495f805
George Kazibwe <gkazibwe@gmail.com>
2023-02-27 14:56:30 -0800

    printing a string followed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
  * _puts - prints a string
  * @str: The string
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
+	while (*str)
 	{
-		_putchar(*str);
-		str++;
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }
