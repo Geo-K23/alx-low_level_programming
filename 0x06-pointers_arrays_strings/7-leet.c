589f179a6ac393e324c493ca6f19114c6abf770e
George Kazibwe <gkazibwe@gmail.com>
2023-03-02 07:51:10 -0800

    leet encoding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ * @str: pointer to a string that will be encoded
+ * Return: returns a pointer to the encoded string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char *leet_letters = "aAeEoOtTlL";
+	char *leet_numbers = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == leet_letters[j])
+			{
+				str[i] = leet_numbers[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
