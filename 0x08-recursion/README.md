150122c0f3537ddd2cd4e9de025ca14f0eee7d1d
George Kazibwe <gkazibwe@gmail.com>
2023-03-08 08:38:14 -0800

    printing a string

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+# Project 0x08. C - Recursion
+0-puts_recursion.c prints a string, followed by a new line
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main.h - contains the prototype functions
+ */
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+
+#endif
