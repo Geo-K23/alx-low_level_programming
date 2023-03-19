8186b1213af766a88430864499daf8b7f744f999
George Kazibwe <gkazibwe@gmail.com>
2023-03-18 23:22:38 -0700

    reallocating a memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,12 +20,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
+	else if (ptr == NULL)
 	{
 		new_ptr = malloc(new_size);
 		return (new_ptr);
 	}
-	if (new_size == old_size)
+	else if (new_size == old_size)
 	{
 		return (ptr);
 	}
