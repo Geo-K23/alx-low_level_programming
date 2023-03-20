194fa95a63b0c7ce7ad839a77d9256e8a39b04ea
George Kazibwe <gkazibwe@gmail.com>
2023-03-20 08:30:29 -0700

    absolute value of a number

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * 3-function_like_macro.h - computes the absolute value of a number x
+ * @x: number whose absolute value is to be computed
+ */
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+#endif
