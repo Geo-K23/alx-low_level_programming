717372e97cde60f4c6d5c4587b6ccec50e393b7e
George Kazibwe <gkazibwe@gmail.com>
2023-03-20 06:22:21 -0700

    define macro named SIZE

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef SIZE
+#define SIZE 1024
+/**
+ * 0-object_like_macro.h - defines a macro named SIZE as an,
+ * abbreviation for the token 1024
+ */
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,2 @@
+# Project 0x0D. C - Preprocessor
+0-object_like_macro.h defines a macro named SIZE as an abbreviation for the token 1024
