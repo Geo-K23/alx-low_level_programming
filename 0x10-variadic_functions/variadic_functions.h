03818edd2b49007f8c1415a070927685b6059ab9
George Kazibwe <gkazibwe@gmail.com>
2023-03-24 04:21:54 -0700

    print anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,21 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 /**
- * sum_them_all - returns the sum of all its parameters
- * @n: is the number of parameters
- * Return: sum of all parameters
+ * struct print - print type with associated print function
+ * @type: indicated data type
+ * @func: function pointer to function to print
  */
+
+typedef struct print
+{
+	char *type;
+	void (*func)(va_list arg);
+} print_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
