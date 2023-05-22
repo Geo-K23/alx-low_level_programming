eec280833b576fcd34b095c743742dade3b1680a
George Kazibwe <gkazibwe@gmail.com>
2023-05-22 10:14:38 -0700

    call c functions in Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of the integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of two integers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: division of two integers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - determines modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: modulus of two integers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
--- 0x18-dynamic_libraries/README.md
+++ 0x18-dynamic_libraries/README.md
@@ -1,4 +1,6 @@
 # 0x18. C - Dynamic libraries
 ## C
-### libdynamic.so
-1-create_dynamic_lib.sh
+libdynamic.so 
+1-create_dynamic_lib.sh  
+100-operations.so 
+101-make_me_win.sh 
